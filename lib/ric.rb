cfe7e0ea962b163c5234797acdd50b0264857cbc
Riccardo GitHubbi <rusko@palladius.it>
2011-12-01 07:45:09 +0000

    repulisti

--- lib/ric.rb
+++ lib/ric.rb
@@ -92,12 +92,6 @@ def _init(explaination='no explaination given')
  $DEBUG ||= false 
  $PROG  = File.basename($0)
 
-  ################################################################################
-  # fake data for some tests :)
- #$a = [1 ,2 ,3, '4', 'cinque', :simbolo ] rescue nil
- #$h = { :key => 'value', :answer => 42 , 'float' => 4.2 , :sym => 'bol', :foo => 'bar1 (symbol)', "foo" => 'bar2 (symbol)' } # rescue nil
- #$s = "This is a test string with some color TBD and some @tags @personal ok?!?"
-  
   ################################################################################	
   # Path to riccardo's Library... Library stuff
 $LOAD_PATH << './lib' 
