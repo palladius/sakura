46393839e49d917b33530fabb023f6b5e29918bb
Riccardo Carlesso (Gic) <riccardo.carlesso@gmail.com>
2012-01-03 15:34:50 +0100

    better docs. Now moving one at a time (slower but easier to integrate with a XARGA I guess)

--- bin/iphoto_big_files.sh
+++ bin/iphoto_big_files.sh
@@ -1,7 +1,14 @@
 #/bin/bash
 
+
+# Usage: 
+#   iphoto_big_files.sh 300M
+#   iphoto_big_files.sh 300M -print0
+
 SIZE=${1:-150M}
+shift
+ADDITIONAL_OPTS=$*
 DIR=~/iPhoto\ Library/Masters/
-echo "# Find big movies in iPhoto dir ($DIR) bigger than \$1 (or $SIZE):" >&2
+echo "# Find big movies in iPhoto dir ($DIR) bigger than $SIZE (additional opts: '$ADDITIONAL_OPTS'):" >&2
 #echo find "$DIR" -iname '*.mov' -size $SIZE
-find "$DIR" -iname '*.mov' -size +$SIZE
+find "$DIR" -iname '*.mov' -size +$SIZE $ADDITIONAL_OPTS
