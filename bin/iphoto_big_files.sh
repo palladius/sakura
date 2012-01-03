3943e219c3ae57a78bcbde05d978922b0e51b15d
Riccardo Carlesso (Gic) <riccardo.carlesso@gmail.com>
2012-01-03 11:41:49 +0100

    better stdout!

--- bin/iphoto_big_files.sh
+++ bin/iphoto_big_files.sh
@@ -2,7 +2,6 @@
 
 SIZE=${1:-150M}
 DIR=~/iPhoto\ Library/Masters/
-echo "# Find big movies in iPhoto dir ($DIR) bigger than \$1 (or $SIZE):"
-echo find "$DIR" -iname '*.mov' -size $SIZE
+echo "# Find big movies in iPhoto dir ($DIR) bigger than \$1 (or $SIZE):" >&2
+#echo find "$DIR" -iname '*.mov' -size $SIZE
 find "$DIR" -iname '*.mov' -size +$SIZE
- # | xargs echo
