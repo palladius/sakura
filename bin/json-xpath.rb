6715df7553ade3cda0b1b778dfc12b546e57ae40
Riccardo GCarlesso <riccardo.carlesso@gmail.com>
2012-09-11 19:30:49 +0100

    sorry, NOW it works

--- bin/json-xpath.rb
+++ bin/json-xpath.rb
@@ -2,6 +2,9 @@
 
 # try also the Oauth playground:
 # http://googlecodesamples.com/oauth_playground/
+#
+# # solves this problem with GCE:
+# curl -s http://metadata/0.1/meta-data/network | python -c "import sys,json; print json.load(sys.stdin)['networkInterface'][0]['accessConfiguration'][0]['externalIp']"
 
 $default_arg = "networkInterface.0.accessConfiguration.0.externalIp"
 
@@ -14,8 +17,10 @@ end
 
 def usage
 	print "Usage: #{$0} <xpath>
+
 Example: #{$0} #{ $default_arg } # this will find your public IP from GCE metadata
-	(Try to use this in pipe with 'curl http://metadata.google.internal/0.1/meta-data/network' "
+	(Try to use this in pipe with 'curl http://metadata.google.internal/0.1/meta-data/network' 
+"
 	exit(1)
 end
 
@@ -27,9 +32,25 @@ def process_args(dotted_arg)
 	my_program = python_program + args.join('')
 
 	#puts python_program_original
-	puts my_program
+	#puts my_program
 	# execute python this
-	system("python \"#{my_program}\"")
+	#old_stdin = $stdin.dup
+	cmd_exec = "/usr/bin/python -c \"#{my_program}\" 2>/dev/null"
+	myin = $stdin.read
+	#print "DEB: #{cmd_exec}\n"
+	#print "INP: #{myin}\n"
+	%x( #{cmd_exec} )
+	#f = IO::popen(cmd_exec,'w')
+	#IO::popen(cmd_exec,'r+')
+	#
+	IO.popen(cmd_exec, mode='w') do |io|
+		#while $stdin.read do
+		#io.write($stdin.read)
+		io.write(myin)
+		#end
+		#io.write mytext
+		#result = io.read
+	end
 end
 	
 def main
