969d758e80e98a556f1a90103ff15120d84d8b7e
Riccardo Carlesso <palladiusbonton+git@gmail.com>
2013-03-21 14:41:20 +0100

    very useful. Found here:
    http://bytes.com/topic/python/answers/458263-days-since-epoch

---
+++ bin/days-since-epoch.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python 
+
+import datetime
+epoch = datetime.datetime.utcfromtimestamp(0)
+print "Epoch: ", epoch
+#1970-01-01 00:00:00
+today = datetime.datetime.today()
+d = today - epoch
+print "Today: ", d
+#13196 days, 9:50:44.266200
+print "Delta: ", d.days # timedelta object
+#13196
+
