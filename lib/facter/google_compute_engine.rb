b4bba8e482f0ed47eb1a33f4cac091b935a64fae
Riccardo Carlesso <palladiusbonton+git@gmail.com>
2013-09-10 09:28:55 +0200

    project id and number on GCE!

--- lib/facter/google_compute_engine.rb
+++ lib/facter/google_compute_engine.rb
@@ -1,10 +1,37 @@
 
 # Riccardo Carlesso
-# So far, iot checks this one: /usr/share/google/safe_format_and_mount
+# So far, it checks this one: /usr/share/google/safe_format_and_mount
+# Methods to catch that:
+# 1. Existence of some fils.
+# 2. /etc/resolv.,conf should match this:
+#    domain c.$PROJECTNAME.google.com.internal.
+#    nameserver 169.254.169.254
+#
 
 Facter.add("is_google_vm") do
   setcode do
     File.exists? '/usr/share/google/safe_format_and_mount'
+    # File.exists? '/usr/share/google/safe_format_and_mount' and `cat /etc/resolv.conf`.split("\n").include?('nameserver 169.254.169.254')
+  end
+end
+
+# if the first line contains this:
+# """domain c.discoproject.google.com.internal."""
+Facter.add("project_name") do
+  setcode do
+    `cat /etc/resolv.conf`.split("\n")[0].split('.')[1]
+  end
+end
+
+Facter.add("project_id") do
+  setcode do
+    `curl 'http://metadata/0.1/meta-data/project-id'`
+  end
+end
+
+Facter.add("project_number") do
+  setcode do
+    `curl 'http://metadata/0.1/meta-data/numeric-project-id'`
   end
 end
 
