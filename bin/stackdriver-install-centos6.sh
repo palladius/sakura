98b97dba4179969f42d66a723a682ef71e7dfef6
Riccardo Carlesso <palladiusbonton+derek.zrh.corp.google.com@gmail.com>
2014-08-13 15:06:05 +0200

    minipatch

--- bin/stackdriver-install-centos6.sh
+++ bin/stackdriver-install-centos6.sh
@@ -6,4 +6,4 @@ STACKDRIVER_API_KEY=${1:-_NOT_SET_}
 echo "Your STACKDRIVER_API_KEY is '$STACKDRIVER_API_KEY'. If its empty please CTRL-C and export it to real value or give it as ARGV[1]" &&
 curl -o /etc/yum.repos.d/stackdriver.repo http://repo.stackdriver.com/stackdriver-el6.repo &&
   yum install stackdriver-agent &&
-  /opt/stackdriver/stack-config --api-key STACKDRIVER_API_KEY
+  /opt/stackdriver/stack-config --api-key "$STACKDRIVER_API_KEY"
