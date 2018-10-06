ad1c7f937812c518a18a6b1a79058276d8259533
Riccardo Carlesso <palladiusbonton@gmail.com>
2018-10-06 18:28:05 +0100

    cleaned up a few things. Added "prepend" for NORM/GPU

--- bin/get-machinetypes-for-region.py
+++ bin/get-machinetypes-for-region.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 
 Sample usage:
@@ -27,6 +28,9 @@ from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 from googleapiclient.errors import HttpError
 
+DEFAULT_REGIONS = "europe-west1,us-central1,asia-east1"
+DEFAULT_MIN_RAM_MB = 1024
+
 """
 {u'creationTimestamp': u'1969-12-31T16:00:00.000-08:00',
  u'description': u'96 vCPUs, 1.4 TB RAM',
@@ -44,7 +48,7 @@ from googleapiclient.errors import HttpError
 """
 
 
-def print_accellerators_per_zone(project_id, zone):
+def print_accellerators_per_zone(prepend, project_id, zone):
   #machine_type_count = 0
   credentials = GoogleCredentials.get_application_default()
   service = discovery.build('compute', 'v1', credentials=credentials) # , access_type=None)
@@ -52,12 +56,12 @@ def print_accellerators_per_zone(project_id, zone):
     request = service.acceleratorTypes().list(project=project_id, zone=zone)
     while request is not None:
         response = request.execute()
-        print "{}:".format(zone) 
+        print "{} {}:".format(prepend, zone) 
         for machine_type in response['items']:
             #pprint(machine_type)
             #if machine_type_count==0:
             #  print "B0 {}:".format(zone)         
-            print "- {0:20} # [GPU] {1}".format(machine_type['name'], machine_type['description'])
+            print "- {0:20} # [{1}] {2}".format(machine_type['name'], prepend, machine_type['description'])
             #machine_type_count += 1
         request = service.acceleratorTypes().list_next(previous_request=request, previous_response=response)
   except KeyError:
@@ -73,38 +77,32 @@ def print_accellerators_per_zone(project_id, zone):
     print('- GPU Zone {} exception ({}): {}'.format(zone, type(e), e))
 
 
-def print_machineTypes_per_zone(project_id, zone, min_ram_megabytes):
-  #machine_type_count = 0
+def print_machineTypes_per_zone(prepend, project_id, zone, min_ram_megabytes):
   credentials = GoogleCredentials.get_application_default()
   service = discovery.build('compute', 'v1', credentials=credentials) # , access_type=None)
   try:
     request = service.machineTypes().list(project=project_id, zone=zone, filter="memoryMb>{}".format(min_ram_megabytes))
     while request is not None:
         response = request.execute()
-        print "{}:".format(zone) 
-        for machine_type in response['items']:
-            #pprint(machine_type)
-            #if machine_type_count==0:
-            #  print "B0 {}:".format(zone)         
-            print "- {0:20} # [NRM] {1}".format(machine_type['name'], machine_type['description'])
-            #machine_type_count += 1
+        print "{} {}:".format(prepend, zone) 
+        for machine_type in response['items']:  
+            print "- {0:20} # [{1}] {2}".format(machine_type['name'], prepend, machine_type['description'])
         request = service.machineTypes().list_next(previous_request=request, previous_response=response)
-  except KeyError:
-    print "- MT EMPTY SEARCH (might have smaller machines)"
+  except KeyError as e:
+    print "- {} EMPTY SEARCH # KeyError (might have smaller machines): {}".format(prepend, e)
   except HttpError as e:
-    #pprint(e)
     match_unknown_zone = re.search('Unknown zone.', "{}".format(e))
     if match_unknown_zone:
-      print "# MT Unknown zone: {}".format(zone)
+      print "# {} Unknown zone: {}".format(prepend, zone)
     else:
-      print "- MT HttpError: {}".format((e)) # .error_details is empty, so _get_reason
+      print "- {} HttpError: {}".format(prepend, e) # .error_details is empty, so _get_reason
   except Exception as e:
-    print('- Zone {} exception ({}): {}'.format(zone, type(e), e))
+    print('- {} Zone {} exception ({}): {}'.format(prepend, zone, type(e), e))
 
 def zones_by_region(region):
   return { "{}-{}".format(region,z) for z in list('abcdefghijklmnopqrstuvwxyz')[:6] }
 
-def print_machinetypes_per_region(project_id, region, min_ram_megabytes):
+def print_machinetypes_and_gpus_per_region(project_id, region, min_ram_megabytes):
   #zones = ['europe-west1-b','europe-west1-d','europe-west1-e']
   zones = zones_by_region(region) # approximate.
   #zones = ['europe-west1-b']
@@ -113,45 +111,48 @@ def print_machinetypes_per_region(project_id, region, min_ram_megabytes):
   print "# MachineTypes in '{reg}' with at least {mbmin}MB in YAML (project '{prj}')".format(reg=region, mbmin=min_ram_megabytes, prj=project_id)
   print "#######################################################################################"
   for zone in zones:
-    print_machineTypes_per_zone(project_id, zone, min_ram_megabytes)
+    print_machineTypes_per_zone("NORM", project_id, zone, min_ram_megabytes)
   print "#######################################################################################"
   print "# GPUs in '{reg}' of any kind in YAML (project '{prj}')".format(reg=region, prj=project_id)
   print "#######################################################################################"
   for zone in zones:
-    print_accellerators_per_zone(project_id, zone)
+    print_accellerators_per_zone("GPU", project_id, zone)
   print
 
 def main():
   parser = OptionParser()
   parser.add_option("-m", "--min-ram-mb", dest="min_ram_megabytes",
-                    default=1024, type="int",
+                    default=DEFAULT_MIN_RAM_MB, type="int",
                     help="minimum RAM (MB)")
   parser.add_option("-r", "--regions", dest="regions",
+                    default=DEFAULT_REGIONS,
                     help="comma-separated regions list")
   parser.add_option("-q", "--quiet",
                     action="store_false", dest="verbose", default=True,
                     help="don't print status messages to stdout")
   parser.add_option("-p", "--project-id", dest="project_id",
-                    #required=True, action="store", type="string",
+                    #required=True, type="string", # action="store", 
                     default=None,
                     help="project id (string)")
   (options, args) = parser.parse_args()
   if options.verbose:
-    print "Verbose is ACTIVE.."
-    print "Options: ", options
-    print "Args: ", args
-    print "ProjectId: ", options.project_id
-    print "Regions: ", options.regions
-  regions = options.regions.split(',')
+    print "[VERBOSE] Verbose is ACTIVE.."
+    print "[VERBOSE] Options: ", options
+    print "[VERBOSE] Args: ", args
+    print "[VERBOSE] ProjectId: ", options.project_id
+    print "[VERBOSE] Regions: ", options.regions
   min_ram_megabytes = options.min_ram_megabytes
   project_id = options.project_id
   if project_id == None:
+    print "Error: ProjectId should not be empty: ", project_id
     parser.print_help()
     exit(1)
-  else:
-    print "ProjectId not empty: ", project_id
+  if options.regions==None:
+    print "Error: Regions should not empty: ", project_id
+    parser.print_help()
+    exit(2)
+  regions = options.regions.split(',')
   for region in regions:
-    print_machinetypes_per_region(project_id, region, min_ram_megabytes)
-  #print_machinetypes_per_region("europe-west3")
+    print_machinetypes_and_gpus_per_region(project_id, region, min_ram_megabytes)
 
 main()
