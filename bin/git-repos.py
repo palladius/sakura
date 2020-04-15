6dbdc7bfbc9349102f1abdd81eee759a9b8b38a5
Riccardo Carlesso <ricc@ricc-macbookpro.corp.google.com>
2020-04-15 12:41:21 +0200

    much more powerful now it does multi-pull :)

--- bin/git-repos.py
+++ bin/git-repos.py
@@ -6,16 +6,37 @@ import sys
 import subprocess
 from optparse import OptionParser
 
-version = '1.1a'
+version = '1.2'
 DEFAULT_DIR = "$HOME/git/"
 default_git_basedir = os.path.expandvars(DEFAULT_DIR)
 debug = False
-valid_commands = [ 'list', 'dump','help']
+valid_commands = [ 'list', 'dump', 'pull-all', 'push-all', 'help' ]
 
 
 def deb(*args):
   if (debug):
     print "#DEB", ' '.join(arg.__str__() for arg in args)
+
+def repos_foreach_execute(mybasedir,dump_basedir,cmd):
+    """ForEach REPO, do: 
+            run(cmd)
+    
+    For the moment i only support git pull
+    """
+    print "repos_foreach_execute(..,..,{})".format(cmd)
+    #print "Repos: ", gitrepos(mybasedir)
+    for ix, repo in enumerate(gitrepos(mybasedir)):
+      #cmd2run = "echo git pull {repo}".format(repo=repo)
+      # This works: git --git-dir $GIC/.git pull
+      cmdArr = ['git', '--git-dir', "{}/.git/".format(repo) , 'pull' ]
+      try:
+          retLong = subprocess.check_output(cmdArr) # , shell=True
+          ret = "[OK] " + repo 
+      except subprocess.CalledProcessError as e:
+          ret = "[ERR] {}: {}".format(repo, e)
+          retLong = "[ERR] Non zero return for: {}: {}".format(repo, e)
+      print "ret: {}".format(ret)
+      #print retLong
 
 def parse_python_26():
 	'''There's a better parser with python2.7 but that's what I have in 2.6..'''
@@ -31,19 +52,23 @@ Default Dir: {DEFAULT_DIR}
 
 """.format(valid_commands=valid_commands,version=version,DEFAULT_DIR=DEFAULT_DIR))
 	parser.add_option("-d", "--dumpdir", dest="dumpdir", default=default_git_basedir,
-	  help="alternate dump dir (default: '{0}')".format(default_git_basedir), metavar="DIR")
+	  help="alternate dump dir (default: '{}')".format(default_git_basedir), metavar="DIR")
 	parser.add_option("-g", "--gitdir", dest="gitdir", default=default_git_basedir,
-	  help="changes git basedir (default: '{0}')".format(default_git_basedir), metavar="DIR")
+	  help="changes git basedir (default: '{}')".format(default_git_basedir), metavar="DIR")
 	parser.add_option("-q", "--quiet",
 		  action="store_false", dest="verbose", default=True,
 		  help="don't print status messages to stdout")
 	parser.add_option("-v", "--version",
-		  help="prints version (which peraltro is '{0}')".format(version))
+		  help="prints version (which peraltro is '{}')".format(version))
 	(options, args) = parser.parse_args()
 	if len(args) != 1:
-	   parser.error("wrong number of arguments: valid_commands={0}".format(valid_commands))
+	   parser.error("wrong number of arguments: I want one (for hour). Valid_commands={}".format(valid_commands))
 	if(args[0] not in valid_commands):
-		parser.error("wrong command: {0}. available:{ 1}".format(args[1], valid_commands))
+		parser.error("Wrong command: '{}'. Available are: {}".format(
+                     args[0],
+                     valid_commands 
+                     ))
+        #exit(9)
 	return (parser,options,args)
 
 def dumprepos(mybasedir,dump_basedir):
@@ -71,25 +96,29 @@ def main():
 	    print repo
 	elif args[0] == 'dump':
 		print dumprepos(opts.gitdir,opts.dumpdir)
+	elif args[0] == 'pull-all':
+		repos_foreach_execute(opts.gitdir,opts.dumpdir,"git pull")
+	elif args[0] == 'push-all':
+		repos_foreach_execute(opts.gitdir,opts.dumpdir,"git push")
 	elif args[0] == 'help':
 		#print dir(parser)
 		print parser.format_help()
 		#parser.print_usage()
 	else:
-		print "Unrecognized command: {0}".format(args[0])
+		print "Unrecognized command: {}".format(args[0])
 		exit(1)
 	exit(0)
 
 def isGitRepo(dir):
 	'''Dice se la sottodir ha un ./.git/ e magari altre cose..'''
-	return os.path.isdir("{0}/.git/".format(dir))
+	return os.path.isdir("{}/.git/".format(dir))
 
 def gitrepos(basedir):
 	'''Dice i repo che sono sotto ~/git/DIR o  ~/git/DIR1/DIR2. Li riconosce dalla dir .git'''
 	repos = []
 	GITGLOB = '.git/'
 	os.chdir(basedir)
-	for files in glob.glob('*/{0}'.format(GITGLOB)) + glob.glob("*/*/{0}".format(GITGLOB)):
+	for files in glob.glob('*/{}'.format(GITGLOB)) + glob.glob("*/*/{}".format(GITGLOB)):
 		repodir = (basedir + files.__str__())[:-len(GITGLOB)] #  removes "/.git/"
 		if isGitRepo(repodir):
 			repos.append(repodir)
