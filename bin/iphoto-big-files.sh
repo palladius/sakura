3c9fc2df5a9ae5f36f80226d5a21899f504a9cc9
Riccardo Carlesso <ricc@ricc-macbookpro.corp.google.com>
2020-10-19 17:55:08 +0200

    iphoto now works with new iphoto paz

--- bin/iphoto-big-files.sh
+++ bin/iphoto-big-files.sh
@@ -4,11 +4,12 @@
 #   iphoto_big_files.sh 300M
 #   iphoto_big_files.sh 300M -print0
 #   iphoto_big_files.sh 300M -print0
-VER=0.9
-SIZE=${1:-150M}
+VER=1.0_2020
+SIZE=${1:-200M}
 shift
 ADDITIONAL_OPTS=$*
-DIR=~/iPhoto\ Library/Masters/
+#2010_DIR=~/iPhoto\ Library/Masters/
+DIR="$HOME/Pictures/Photos Library.photoslibrary/"
 
 function usage() {
   echo "$0 v$VER"
@@ -17,12 +18,14 @@ function usage() {
   echo '
    iphoto_big_files.sh -h                 # provides help
    iphoto_big_files.sh 300M               # finds files bigger than 300M
+   iphoto_big_files.sh 2G                 # GB also supported :) 
    iphoto_big_files.sh 300M -print0       #  .. and prints them in a more "xargsable" way
    iphoto_big_files.sh --dest DESTDIR     # to be implemented yet :(
 More elaborate:
    iphoto_big_files.sh 400M -print0 | xargs -0 mvto /media/myhd/bigvideos/
    iphoto_big_files.sh 100M  | while read F ; do mv -n "$F" /Volumes/MyDisk/MyDir/; done # moves without overwriting
 '
+#iphoto_big_files.sh --dest DESTDIR     # to be implemented yet :(
 }
 #iphoto_big_files.sh 60M | while read t ; do mv -n "$t" /Volumes/FreeAgent\ GoFlex\ Drive/important-backups/; done
 
@@ -32,6 +35,10 @@ if echo "$SIZE" | fgrep -q -- "-h" ; then
 	exit 1
 fi
 
+yellow "Call me with -h to fins out more ;)"
 echo "# Find big movies in iPhoto dir ($DIR) bigger than $SIZE (additional opts: '$ADDITIONAL_OPTS'):" >&2
+echo "Total Size: $(du -sh "$DIR")"
 #echo find "$DIR" -iname '*.mov' -size $SIZE
-find "$DIR" -iname '*.mov' -size +$SIZE $ADDITIONAL_OPTS
+#find "$DIR" -iname '*.mov' -size +$SIZE $ADDITIONAL_OPTS
+find "$DIR"  -size +$SIZE $ADDITIONAL_OPTS
+
