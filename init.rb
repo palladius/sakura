4d54c552b67fac937db44a644cd3c19b3f3eb4b1
Riccardo Carlesso <palladiusbonton@gmail.com>
2026-01-17 14:06:12 +0100

    Merged changes from stash on Windows

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@
 # So this will also work as a plugin! See
 # http://asciicasts.com/episodes/135-making-a-gem
 
-#require 'sakuric'
+
 
 require File.expand_path(File.dirname(__FILE__) + '/lib/init.rb' ) 
 
